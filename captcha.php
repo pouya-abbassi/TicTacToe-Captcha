9f920fa44bf5789d0009f2ae1c6c345122f2d037
pouya-abbassi <virus.rvs@gmail.com>
2015-10-19 21:09:37 +0330

    Adding style.css file and moving inline styles to it. More readable codes in captcha.php file.

--- captcha.php
+++ captcha.php
@@ -1,38 +1,30 @@
-<h3>TicTacToe Captcha</h3>
-<p>The captcha that doesn't hurt!</p>
-<br>
-<table id="table" border="0">
-	<tr>
-		<thead>You are "O" and it's your turn.<br>Tap on the right place to win!</thead>
-	</tr>
-	<tr>
-		<td id="a1"><button type="button" value="asdf"> </button></td>
-		<td id="a2"><button type="button" value="asdf"> </button></td>
-		<td id="a3"><button type="button" value="asdf">O</button></td>
-	</tr>
-	<tr>
-		<td id="b1"><button type="button" value="asdf">X</button></td>
-		<td id="b2"><button type="button" value="asdf"> </button></td>
-		<td id="b3"><button type="button" value="asdf">O</button></td>
-	</tr>
-	<tr>
-		<td id="c1"><button type="button" value="asdf">O</button></td>
-		<td id="c2"><button type="button" value="asdf">X</button></td>
-		<td id="c3"><button type="button" value="asdf">X</button></td>
-	</tr>
-</table>
-
-<style type="text/css">
-	button{
-		width: 32px;
-		height: 32px;
-	}
-	*{
-		text-align: center;
-		margin: auto;
-		padding: 0px;
-	}
-	h3{
-		padding-top: 10px;
-	}
-</style>
+<head>
+	<meta charset="UTF-8">
+	<title>TicTacToe Captcha</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h3>TicTacToe Captcha</h3>
+	<p>The captcha that doesn't hurt!</p>
+	<br>
+	<table id="table" border="0">
+		<tr>
+			<thead>You are "O" and it's your turn.<br>Tap on the right place to win!</thead>
+		</tr>
+		<tr>
+			<td id="a1"><button type="button" value="asdf"> </button></td>
+			<td id="a2"><button type="button" value="asdf"> </button></td>
+			<td id="a3"><button type="button" value="asdf">O</button></td>
+		</tr>
+		<tr>
+			<td id="b1"><button type="button" value="asdf">X</button></td>
+			<td id="b2"><button type="button" value="asdf"> </button></td>
+			<td id="b3"><button type="button" value="asdf">O</button></td>
+		</tr>
+		<tr>
+			<td id="c1"><button type="button" value="asdf">O</button></td>
+			<td id="c2"><button type="button" value="asdf">X</button></td>
+			<td id="c3"><button type="button" value="asdf">X</button></td>
+		</tr>
+	</table>
+</body>
