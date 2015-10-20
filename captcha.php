1fdc57a0df1af201e57139155270cf80ebda8eb4
pouya-abbassi <virus.rvs@gmail.com>
2015-10-20 20:41:35 +0330

    Some little changes on code to make it standard, nice and clean. Comment added to code where needs to. Everyone can understand the code now ;)

--- captcha.php
+++ captcha.php
@@ -1,9 +1,9 @@
-<?php
-if (1 > $_GET["q"] || $_GET["q"] > 9) {
+<?php   //output of this page will be displayed in bottom of captcha
+if (1 > $_GET["q"] || $_GET["q"] > 9) {	//check if the q is between 1 to 9 (security check)
 	echo "Wrong answer.";
-}elseif ( $_GET["q"] == 5) {
+}elseif ( $_GET["q"] == 5) {	//check if q is the right answer. this will be changed as soon as we make database and sample games
 	echo "Awesome!";
-}else {
+}else {	// if q was not the right answer.
 	echo "Wrong answer.";
 }
 ?>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	<p>The captcha that doesn't hurt!
 	<br><br>
 	You are "O" and it's your turn.<br>Tap on the right place to win!</p>
-	<table id="table" border="0">
+	<table id="table" border="0">	<!-- one table for positioning all buttons -->
 		<tr>
 			<td id="a1"><button type="button" value="1"> </button></td>
 			<td id="a2"><button type="button" value="2"> </button></td>
@@ -29,7 +29,8 @@
 			<td id="c3"><button type="button" value="9">X</button></td>
 		</tr>
 	</table>
-	<p id="answer"></p>
+	<p id="answer"></p>	<!-- the resault of ajax will be displayed here -->
+	<br>
+	<button id='retry' type='button' style='width:auto; display:none;' onclick="$('button').prop('disabled', false);">retry</button>	<!-- retry button. hidden by default. inline js script to make buttons reusable. -->
 </body>
 </html>
-<br><button id='retry' type='button' style='width:auto; display:none;' onclick="$('button').prop('disabled', false);">retry</button>
